333955793e89d2018c42cee5efc6a2a13c589152
deepaks20 <deepaksidhardh20@gmail.com>
2023-02-22 15:28:01 +0530

    first commit

---
+++ src/main/java/com/example/demo/ColorgameApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorgameApplication.class, args);
+	}
+
+}
